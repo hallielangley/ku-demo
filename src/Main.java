0916046237baa6ccf8493c0fec357a2f40f90796
hallielangley <hallielangley@gmail.com>
2023-12-04 12:00:10 -0500

    Final changes.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // EXAMPLE 1: "Throw it somewhere else."
+        File file = new File("src/message.txt");
+        Scanner message = new Scanner(file);
+        System.out.println(message.nextLine());
+
         String name = null;
         int age = -1;
 
-        // Get the user's name
+        // EXAMPLE 3: Bypass bad behaviour.
         System.out.println("Hello! What is your name? ");
         Scanner nameIn = new Scanner(System.in);
         name = nameIn.next();
+        validateName(name);
         System.out.println("Hi " + name + "!");
 
-        // Get the user's age
+        // EXAMPLE 2: Handle it with a try-catch.
         System.out.println("How old are you?");
         Scanner ageIn = new Scanner(System.in);
         age = ageIn.nextInt();
         System.out.println("That's neat, " + age + " is a good age.");
+    }
+
+    static void validateName(String name) {
+        // Throw an exception if the name contains profanity.
     }
 }
--- src/MainFinal.java
+++ src/MainFinal.java
@@ -1,24 +1,33 @@
+import javax.naming.InvalidNameException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainFinal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // EXAMPLE 1: "Throw it somewhere else."
+        File file = new File("src/message.txt");
+        Scanner message = new Scanner(file);
+        System.out.println(message.nextLine());
+
         String name = null;
         int age = -1;
 
-        // Get the user's name
+        // EXAMPLE 3: Throw it, and handle with a try-catch.
         System.out.println("Hello! What is your name? ");
         try {
             Scanner nameIn = new Scanner(System.in);
             name = nameIn.next();
             validateName(name);
-        } catch (BadNameException e) {
+        } catch (InvalidNameException e) {
             name = "[REDACTED]";
             System.out.println("**" + e.getMessage() + "**");
         }
         System.out.print("Hi " + name + "! ");
 
-        // Get the user's age
+        // EXAMPLE 2: Handle it with a try-catch.
         System.out.println("How old are you?");
         while (true) {
             try {
@@ -26,23 +35,18 @@ public class MainFinal {
                 age = ageIn.nextInt();
                 break;
             } catch (InputMismatchException e) {
-                System.out.println("Are you sure that was an age? Try again.");
+                System.out.println("Try again.");
             }
         }
         System.out.println("That's neat, " + age + " is a good age.");
 
     }
 
-    static void validateName(String name) throws BadNameException {
-        if (name.equals("bad")) {
-            throw new BadNameException("Name redacted for profanity.");
+    static void validateName(String name) throws InvalidNameException {
+        if (name.contains("profanity")) {
+            // Throw an exception if the name contains profanity.
+            throw new InvalidNameException("Name redacted for profanity.");
         }
     }
 
-    static class BadNameException extends Exception {
-
-        public BadNameException(String message) {
-            super(message);
-        }
-    }
 }
